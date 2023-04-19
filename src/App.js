e2634f4fda589232e9e0d161433a7f7beab4bd4e
ragab <ragab@legend>
2023-04-20 01:38:20 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Link, Route, Routes } from 'react-router-dom';
 
 import './App.css';
 import Projects from './pages/projects/projects';
