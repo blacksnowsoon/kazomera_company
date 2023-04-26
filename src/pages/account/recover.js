3adcf4967e98e1e8e081190bc81763e093dd602c
ragab <ragab@legend>
2023-04-26 16:44:20 +0200

    simple

---
+++ src/pages/account/recover.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default function Recover() {
+	return (
+    <>
+      <h1 className='heading'>Recover the account</h1>
+      <form className='form recover__form'>
+        <input placeholder='Recover by email' type='email' />
+        <button>Submit</button>
+      </form>
+      <Link to="../login">Have an account? Login Now!</Link>
+      <Link to="../signup">Need an account? Singup Now!</Link>
+    </>
+	)
+}
